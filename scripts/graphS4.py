0c3d85820786264ae8838e3dce73b02ad030c076
georgesteve <37008877+georgesteve@users.noreply.github.com>
2022-02-17 18:48:39 -0500

    Create graphS4.py

---
+++ scripts/graphS4.py
@@ -0,0 +1,33 @@
+import os
+import datetime
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+
+from datetime import datetime, timedelta
+import calendar
+
+
+
+
+class S4:
+    """
+    Define un rectángulo según su base y su altura.
+    """
+    def __init__(self):
+        date = date.today() 
+        self.d1  = date - timedelta(days = int(date.strftime("%d"))-1)
+        self.d2 = date.replace(day = calendar.monthrange(date.year, date.month)[1])
+
+    def muestra(self):
+        return self.d1 , self.d2
+
+limite = S4()
+print("Área del rectángulo: ", limite.muestra())
